e806a62cd64c32f9d27cd7ab52b3e010a50c99c7
Berkin Akkaya <berkin_akkaya@hotmail.com>
2025-03-02 17:00:13 +0300

    move and look

--- Player.gd
+++ Player.gd
@@ -1,21 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 400
+var direction = Vector2.ZERO
 
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-	input_direction.y = 0
-
-	if position.x < -200 && input_direction.x < 0:
-		input_direction.x = 0
-	if position.x > 200 && input_direction.x > 0:
-		input_direction.x = 0
-	
-	return input_direction
+func _input(event):
+	if event is InputEventMouseButton:
+		var mousePosition = get_global_mouse_position()
+		direction = position.direction_to(mousePosition)
+		look_at(get_global_mouse_position())
 
 func _physics_process(_delta):
-	var input_direction = get_input()
-	velocity = input_direction * speed
-
+	velocity = direction * 50
 	move_and_slide()
